10816f6857214872fe5782693de6117185e42829
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2019-06-11 16:17:46 +0530

    list note screen and edit/add note screen implemented

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sqflite_demo/screens/note_detail.dart';
+import 'package:flutter_sqflite_demo/screens/note_list.dart';
+
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'NoteKeeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple
+      ),
+      home: NoteList(),
+    );
+  }
+}
